cb67a3c5a0613dec2f03de8514fc013e8ec6e492
Skybladev2 <qlzp2@mail.ru>
2012-08-06 23:11:04 +0400

    Figured out how to 'correctly' destroy local objects. However, server objects network instantiation is buffered, so every time client connects to server it produces 2 copies: 1 buffered from the server (as server does not know this is the same client) and 1 by client itself. Must find a way to destroy server objects locally.

--- Assets/Instantiate.cs
+++ Assets/Instantiate.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Instantiate : MonoBehaviour
 {
     public Transform SpaceCraft = null;
+
 
     // Use this for initialization
     void Start()
@@ -20,11 +22,39 @@ public class Instantiate : MonoBehaviour
     void OnNetworkLoadedLevel()
     {
         // Instantiating SpaceCraft when Network is loaded
-        Network.Instantiate(SpaceCraft, transform.position, transform.rotation, 0);
+		Network.Instantiate(SpaceCraft, transform.position, transform.rotation, 0);
+
+		if(Network.isClient)
+			Debug.Log("Instaniated on client");
+
+		if(Network.isServer)
+			Debug.Log("Instaniated on server");
+
     }
     void OnPlayerDisconnected(NetworkPlayer player)
     {
+		Network.DestroyPlayerObjects(player);
         Network.RemoveRPCs(player, 0);
-        Network.DestroyPlayerObjects(player);
+        
     }
+
+	void OnNetworkInstantiate(NetworkMessageInfo info) {
+        Debug.Log("New object instantiated by " + info.sender);
+    }
+
+	void OnDisconnectedFromServer(NetworkDisconnection info)
+	{
+		//foreach (var obj in viewIDs) {
+		//	Network.Destroy(obj);
+		//}
+
+		//foreach (var obj in objects) {
+
+			// уничтожаем клиентский объект	
+			Destroy(GameObject.Find("Player(Clone)")); 
+			// уничтожаем себя, но после первого вызова мы не сможем себя создавать
+			// Network.Instantiate на клиенте перестанет работать
+			//Destroy(gameObject); 
+		//}
+	}
 }
